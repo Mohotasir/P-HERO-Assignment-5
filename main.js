6acb299252dcfb2fdf5c7e4d3efa84d1a8775051
mohotasir <talukdertahsin643@gmail.com>
2024-02-19 17:25:44 +0600

    added js codes

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const bookingSecID = document.getElementById('booking-section');
 document.getElementById('buy-ticket-btn').addEventListener('click',()=>{
     bookingSecID.scrollIntoView({ behavior: 'smooth' });
 });
+document.getElementById('menu-btn').addEventListener('click',()=>{
+    bookingSecID.scrollIntoView({ behavior: 'smooth' });
+});
 //initial values
 let totalSeat = 40;
 let clickOnSeat = 0;
@@ -14,23 +17,29 @@ const seats = document.querySelectorAll('.sit');
 const buyingSeatCount = document.getElementById('count-seat');
 const totalTicketPrice = document.getElementById('total-price');
 const totalDiscountPrice = document.getElementById('grand-price');
+const copunDiv = document.getElementById('cupon-div');
+const applyCupon = document.getElementById('apply-cupon');
+const grandPriceDiv = document.getElementById('grand-price-div');
+const phoneField = document.getElementById('phone-field');
+const nextBtn = document.getElementById('next-btn');
 
 const bookingSeats =[];
 for(const seat of seats){
     seat.addEventListener('click',()=>{
         const seatName = seat.innerHTML;
         
-        totalSeat--;
+        
         //sit limitation handle
         if(clickOnSeat<4){
                 if(bookingSeats.includes(seatName)){
                     alert("This seat is already selected !!!! ");
                 }else{
+                    totalSeat--;
                     bookingSeats.push(seatName);
                     clickOnSeat++;
                     buyingSeatCount.innerText = clickOnSeat;
-                    seat.classList.toggle('bg-green-500')
-                    seat.classList.toggle('hover:bg-green-500');
+                    seat.classList.toggle('bg-green-300')
+                    seat.classList.toggle('hover:bg-green-300');
                     const div = document.createElement('div');
                     const p1 = document.createElement('p');
                     const p2 = document.createElement('p');
@@ -47,6 +56,10 @@ for(const seat of seats){
                     totalPrice += intPrice;
                     totalTicketPrice.innerText = totalPrice;
                     totalDiscountPrice.innerText = totalPrice;
+                    if(clickOnSeat === 4){
+                       copunDiv.children[0].disabled = false;
+                       copunDiv.children[1].disabled = false;
+                    }
             }
    
         }else{
@@ -55,5 +68,33 @@ for(const seat of seats){
             
         }
         seatCount.innerText = totalSeat;
+        
     })
+    
+    
 }
+applyCupon.addEventListener('click',()=>{
+    if(copunDiv.children[0].value === "NEW15"){
+        totalDiscountPrice.innerText = totalPrice - (totalPrice*.15);
+        copunDiv.classList.add('hidden');
+        grandPriceDiv.classList.add('bg-green-300')
+    }else if(copunDiv.children[0].value === "Couple 20"){
+        totalDiscountPrice.innerText = totalPrice - (totalPrice*.20);
+        copunDiv.classList.add('hidden');
+        grandPriceDiv.classList.add('bg-green-300')
+    }else{
+        const err = document.getElementById('err-msg');
+        err.classList.add('text-red-700');
+        err.innerText = "Wrong coupon !";
+    }
+})
+
+//active next button
+
+phoneField.addEventListener('keyup',()=>{
+    if(bookingSeats.length > 0 && phoneField.value !== null && phoneField.value.trim() !== '' ){
+        nextBtn.disabled  = false;
+    }else{
+        nextBtn.disabled = true;
+    }
+})
